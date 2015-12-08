27e167c80b5f46429eca713816147121e10f5b01
Your Name <you@example.com>
2015-12-08 08:34:41 +0000

    fix for login controller

--- config/routes.rb
+++ config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
     end
   end
   
-  get 'login' => 'users#login'
+  get 'login' => 'api/users#login'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
