9693d63a306a24b585c9dd561c0065f96507a502
Your Name <you@example.com>
2015-12-09 10:11:35 +0000

    test

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,14 +12,12 @@ class ApplicationController < ActionController::Base
   end
 
   def cors_preflight_check
-    if request.method == 'OPTIONS'
+    head(:ok) if request.request_method == "OPTIONS"
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
       headers['Access-Control-Allow-Headers'] = 'X-Requested-With, X-Prototype-Version, Token'
       headers['Access-Control-Max-Age'] = '1728000'
-
       render :text => '', :content_type => 'text/plain'
-    end
   end
   
 protected
